1c49aeada306535ddcaaedfb34d0aa85d7a2dff4
VaibhavJain0101 <jvaibhav7470@gmail.com>
2026-04-11 00:16:26 +0530

    Update server log message to use dynamic port

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 3000
 connectToDB()
 
 app.listen(port,  () => {
-        console.log("server is running on port 3000")
+       console.log(`Server is running on port ${port}`)
 
     })
