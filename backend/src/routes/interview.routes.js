f2edd79869956ff4f712017b857b6232f9f6631f
VaibhavJain0101 <jvaibhav7470@gmail.com>
2026-04-11 15:20:01 +0530

    Fix import case for interview.controller

--- backend/src/routes/interview.routes.js
+++ backend/src/routes/interview.routes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const authMiddleware = require("../middlewares/auth.middleware")
-const interviewController = require("../controllers/interview.controller")
+const interviewController = require("../controllers/interview.Controller")
 const upload = require("../middlewares/file.middleware")
 
 const interviewRouter = express.Router()
