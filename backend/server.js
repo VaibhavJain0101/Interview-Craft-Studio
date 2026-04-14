9fe4218a1a96b24661fe4daac619ae04ec299f5d
vaibhavjain0101 <jvaibhav7470@gmailcom>
2026-04-15 01:17:24 +0530

    change server port to 10000

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ const connectToDB = require("./src/config/database")
 
 connectToDB()
 
+const PORT = process.env.PORT || 3000
 
-app.listen(10000, () => {
-    console.log("Server is running on port 10000")
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
